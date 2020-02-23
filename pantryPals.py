94e05884ab47268e845f998c07174f6100a40540
shreyatheslaya <shreyashankar116@gmail.com>
2020-02-22 19:00:31 -0500

    changes

--- pantryPals.py
+++ pantryPals.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask import render_template
 from markupsafe import escape
-app = Flask(__name__)
-
-url_for('static', filename='/static/css/main.css')
 
 @app.route('/')
 def hello_world():
